17848b7cd56cb0e15e6373e4421ef3af76a7efb9
unknown <rayssared28@outlook.com>
2021-05-31 16:10:16 -0300

    primeira versão

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um valor"))
+b = int(input("Digite o segundo valor"))
+operacao = input("+: soma\n -:subtração")
+if operacao =='+':
+    resultado = a + b
+else:
+    resultado = a - b 
+print(resultado)
